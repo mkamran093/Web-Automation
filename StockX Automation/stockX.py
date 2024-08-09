16f63de3264da34e153cafa9843a8a4f3e8fcbf1
Muhammad Kamran <mkamran.bscssef20@iba-suk.edu.pk>
2024-08-09 22:31:56 +0500

    Bot Detection Feature not completed

--- StockX Automation/stockX.py
+++ StockX Automation/stockX.py
@@ -4,8 +4,8 @@ import json
 import logging
 import psutil
 import pickle
+import pyautogui
 import undetected_chromedriver as uc
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -28,76 +28,35 @@ for proc in psutil.process_iter(['pid', 'name']):
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
         pass
 
-EMAIL = "email address"
-PASSWORD = "password"
-COOKIES_FILE = "cookies.pkl"
-webdriver_path = "chromedriver.exe"
 
-# url = input("Enter the product url: ")
-# size = input("Size:  ")
-url = 'https://stockx.com/timberland-6-inch-premium-waterproof-wheat'
-size = '5.5'
+EMAIL = "kamranbangash@nexbit.pk"
+PASSWORD = "Kamran123."
+webdriver_path = "undetected_chromedriver.exe"
+url = input("Enter the product url: ")
+size = input("Size:  ")
+# url = 'https://stockx.com/timberland-6-inch-premium-waterproof-wheat'
+# size = '5.5'
+
 # Set up Chrome options
 options = uc.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--user-data-dir=C:\\Users\\NeXbit\\AppData\\Local\\Google\\Chrome\\User Data")
+# options.add_experimental_option("excludeSwitches", ["enable-automation"])
+# options.add_experimental_option('useAutomationExtension', False)
+
 
 # Initialize Chrome driver
-driver = uc.Chrome(executable_path=webdriver_path, options=options)
-
-# Define the cookies file
-# cookies_file = "cookies.json"
-
-
-def save_cookies(driver):
-    cookies = driver.get_cookies()
-    pickle.dump(cookies, open("cookies.pkl", "wb"))
-
-# Function to load cookies
-def load_cookies(driver):
-    cookies = pickle.load(open("cookies.pkl", "rb"))    
-    for cookie in cookies:
-        cookie['domain'] = '.stockx.com'
-        try:
-            driver.add_cookie(cookie)
-        except Exception as e:
-            print(e)
-
-
-def check_cookies(driver):
-    if os.path.isfile("cookies.pkl"):
-        load_cookies(driver)
-        driver.refresh()
-        logger.info("Loaded cookies.")
-        return
-    else:
-        login_to_stockx(driver, EMAIL, PASSWORD)
-
-
-def login_to_stockx(driver, email, password):
-    driver.get('https://stockx.com/login')
-    check_bot(driver)
-    try:
-        time.sleep(5)
-        email = driver.find_element(By.ID, "email-login")
-        email.send_keys(EMAIL + Keys.TAB)
-
-        password = driver.find_element(By.ID, "password-login")
-        password.send_keys(PASSWORD + Keys.RETURN)
-        time.sleep(5)
-        # save_cookies(driver)
-        check_bot(driver)
-        logger.info("Logged in successfully.")
-    except TimeoutException as e:
-        logger.error("Login failed: %s", e)
-        login_to_stockx(driver, email, password)
-        raise 
+driver = uc.Chrome( options=options)
 
 def navigate_to_product_page(driver):
+    """
+    Navigate to the product page, handling any errors or bot verification challenges.
+    """
     while True:
         try:
+            logger.info(f"Navigating to product page")
             driver.get(url + "?defaultBid=false")
             logger.info("Page found.")
-            check_bot(driver)
             break
         except Exception as e:
             logger.error("Page not found: %s", e)
@@ -109,15 +68,24 @@ def navigate_to_product_page(driver):
             elif cont != 'y':
                 logger.warning("Invalid input. Please enter 'y' or 'n'.")
 
+
 def bid_price(driver, url, size):
+    """
+    Determine the bid price based on the current ask price.
+    """
     try:
         ask_price = driver.find_element(By.CSS_SELECTOR, "h2[data-testid='trade-box-buy-amount']").text
         ask_price = ''.join([c for c in ask_price if c.isdigit() or c == '.'])
         return int(ask_price) * 0.7
     except:
+        logger.info("Visiting bidding page to determine bid price...")
         visit_bidding_page(driver, url, size)
 
+
 def get_last_sale_price(driver, url, size):
+    """
+    Retrieve the last sale price for the product.
+    """
     time.sleep(5)
     try:
         last_sale_element = driver.find_element(By.CSS_SELECTOR, 'div[data-component="LastSale"]')
@@ -131,88 +99,105 @@ def get_last_sale_price(driver, url, size):
         logger.error("Last sale Amount not Available, Entering Custom price")
         price_text = bid_price(driver, url, size)
         return price_text
+    
 
 def visit_bidding_page(driver, url, size):
+    """
+    Navigate to the bidding page for the product.
+    """
     url = url.replace('stockx.com', 'stockx.com/buy')
     url += "?size=" + size
     try:
+        logger.info(f"Navigating to bidding page")
         driver.get(url)
-        check_bot(driver)
+        # Check for bot verification
+        logger.info("Checking for bot verification on bidding page...")
+        # check_bot(driver)
         logger.info("Page found.")
     except TimeoutException as e:
         logger.error("Page not found: %s", e)
         driver.quit()
         exit()
-    
+
 
 def place_bid(driver, price_text):
-    
+    """
+    Place a bid on the product.
+    """
     try:
         time.sleep(5)
-        WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "input.chakra-input"))
+        logger.info("Waiting for bid input field to be present...")
+        bid = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input.chakra-input"))
         )
-        bid = driver.find_element(By.CSS_SELECTOR, "input.chakra-input")
         bid.send_keys(Keys.CONTROL + "a")
         bid.send_keys(Keys.DELETE)
         bid.send_keys(price_text)
         time.sleep(5)
     except:
-        print("Option for inputting Bid price not found, Quitting program")
+        logger.error("Option for inputting Bid price not found, Quitting program")
         driver.quit()
         exit()
-
+   
+    try:
+        logger.info("Checking for 'Review Bid' or 'Next' button...")
+        # Locate the 'Review Bid' button
+        review = driver.find_element(By.XPATH, "//*[text()='Review Bid']")
+    except:
+        try:
+            # If 'Review Bid' is not found, locate the 'Next' button
+            logger.info("Review Bid button not found, checking for 'Next' button...")
+            review = driver.find_element(By.XPATH, "//*[text()='Next']")
+        except TimeoutException as e:
+            logger.error("Neither 'Review Bid' nor 'Next' button was found: %s", e)
+            driver.quit()
+            exit()
 
     try:
-        review = driver.find_element(By.XPATH, "//*[text()='Review Bid']")
         review.click()
         time.sleep(5)
-    except TimeoutException as e:
-        logger.error("Review Bid button not found: %s", e)
+    except Exception as e:
+        logger.error("Failed to click the button: %s", e)
         driver.quit()
         exit()
 
+
     try:
         time.sleep(5)
-        WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//*[text()='Confirm Bid']"))
+        logger.info("Waiting for 'Confirm Bid' button to be present...")
+        confirm = WebDriverWait(driver, 15).until(
+            EC.visibility_of_element_located((By.XPATH, "//*[text()='Confirm Bid']"))
         )
-        confirm = driver.find_element(By.XPATH, "//*[text()='Confirm Bid']")
         confirm.click()
         logger.info("Bid placed successfully.")
+        time.sleep(5)
+
     except TimeoutException as e:
         logger.error("Confirm Bid button not found: %s", e)
         driver.quit()
         exit()
-    
-    # time.sleep(5)
-    # WebDriverWait(driver, 5).until(
-    #     EC.presence_of_element_located((By.XPATH, "//*[text()='Confirm Bid']"))
-    # )
-    # confirm = driver.find_element(By.XPATH, "//*[text()='Confirm Bid']")
-    # confirm.click()
 
-def check_bot(driver):
-    try:
-        hold = driver.find_element(By.XPATH, "//*[text()='Press & Hold']")
-        actions = ActionChains(driver)
-        actions.click_and_hold(hold).perform()
-        time.sleep(5)
-    except:
-        pass
+def click_and_hold():
+    button_location = pyautogui.locateCenterOnScreen("C:\\Users\\NeXbit\\Desktop\\Selenium\\StockX Automation\\button.png")
+    if button_location:
+        pyautogui.moveTo(button_location)
+        pyautogui.mouseDown()
+        time.sleep(15)
+        pyautogui.mouseUp()
 
 def main():
     try:
-        # check_cookies(driver)
-        login_to_stockx(driver, EMAIL, PASSWORD)
+        logger.info("Navigating to product page...")
         navigate_to_product_page(driver)
+        logger.info("Retrieving last sale price...")
         last_sale_price = get_last_sale_price(driver, url, size)
+        logger.info("Visiting bidding page...")
         visit_bidding_page(driver, url, size)
+        logger.info("Placing bid...")
         place_bid(driver, last_sale_price)
     except Exception as e:
         logger.error("An error occurred: %s", e)
     finally:
         driver.quit()
-
 if __name__ == "__main__":
     main()
